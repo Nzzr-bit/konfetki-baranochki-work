7f52822ef9aed03c5e29e85eb0762a4a8d524b5e
Nazar Pilipchuk <nazar.pilipchuk.2015@gmail.com>
2024-07-08 17:33:00 +0300

    little changes

--- components/KnowUs.tsx
+++ components/KnowUs.tsx
@@ -1,5 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 export function KnowUs() {
-  return <div className="container mx-auto">KnowUs</div>;
+  return (
+    <div className="container mx-auto">
+      <h2 className="text-ml font-semibold mb-16">ЗНАКОМЬСЯ С НАМИ</h2>
+      <div className="flex items-start">
+        <Image
+          src="/KnowUsBanner.png"
+          alt="Знакомся с нами"
+          width={949}
+          height={560}
+          className="mr-8"
+        />
+        <div className="">
+          <button className="px-33 bg-main-red">22</button>
+          <button className="px-33 bg-main-red">22</button>
+          <button className="px-33 bg-main-red">22</button>
+        </div>
+      </div>
+    </div>
+  );
 }
